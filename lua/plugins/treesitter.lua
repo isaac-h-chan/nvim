e9059d88c3fd8f84f5865447b0edb5d9229ddab4
Isaac Chan <isaacchancollege@gmail.com>
2024-05-08 19:16:57 -0700

    harpoon

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,9 @@ return {
     config = function()
 	  local configs = require("nvim-treesitter.configs")
 	  configs.setup({
-		  ensure_installed = { "c", "lua", "python", "javascript", "html" },
+		  ensure_installed = { "c", "lua", "python", "javascript", "html", "prisma" },
 		  highlight = { enable = true },
-		  indent = { enable = true },  
+		  indent = { enable = true },
 		})
     end,
 }
