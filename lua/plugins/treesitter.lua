ac3c37dfbe5e92e4f750d341de1109a22e7e84c3
Isaac <isaacchancollege@gmail.com>
2024-06-17 12:12:45 -0700

    terraform treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = { "c", "lua", "python", "javascript", "html", "prisma" },
+		configs.setup({ ensure_installed = { "c", "lua", "python", "javascript", "html", "prisma", "terraform" },
 			highlight = { enable = true },
 			indent = { enable = true },
 			incremental_selection = {
