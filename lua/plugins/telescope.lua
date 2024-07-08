52d54b113abb08120b78e0a11cf470856367fc30
Isaac <isaacchancollege@gmail.com>
2024-07-08 16:37:36 -0700

    added more keybinds for more telescope.nvim pickers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,9 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader{fgf}', builtin.git_files, {})
+			vim.keymap.set('n', '<leader>fgr', builtin.live_grep, {})
+			vim.keymap.set('n', "<leader>fgs", builtin.git_status, {})
 			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 			vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
@@ -19,6 +21,12 @@ return {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown {
 						}
+					}
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+						no_ignore = true
 					}
 				}
 			}
