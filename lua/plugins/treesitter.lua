9e7df8478cb7904fe15d26e2f737e669bb335a0e
Isaac <isaacchancollege@gmail.com>
2024-07-10 11:55:34 -0700

    added more parsers to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
-		configs.setup({ ensure_installed = { "c", "lua", "python", "javascript", "html", "prisma", "terraform" },
+		configs.setup({ ensure_installed = { "c", "lua", "python", "javascript", "html", "prisma", "terraform", "markdown", "vim", "vimdoc"},
 			highlight = { enable = true },
 			indent = { enable = true },
 			incremental_selection = {
