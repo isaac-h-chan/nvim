c1336c62f14c046c16908469bb8d73a9bc993166
Isaac Chan <isaacchancollege@gmail.com>
2025-08-29 21:58:00 -0700

    update markview.nvim

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,12 +1,7 @@
 return {
-    "OXY2DEV/markview.nvim",
-    ft = "markdown",
+	"OXY2DEV/markview.nvim",
+	lazy = false,
 
-    dependencies = {
-        -- You may not need this if you don't lazy load
-        -- Or if the parsers are in your $RUNTIMEPATH
-        "nvim-treesitter/nvim-treesitter",
-
-        "nvim-tree/nvim-web-devicons"
-    },
-}
+	-- For `nvim-treesitter` users.
+	priority = 49,
+};
