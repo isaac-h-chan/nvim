fd6d2482264970408214b835ab3267dce689671b
Isaac Chan <isaacchan@Isaacs-MacBook-Pro.local>
2026-06-02 13:40:36 -0700

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
+		local configs = require("nvim-treesitter.config")
 		configs.setup({ ensure_installed = { "c", "lua", "python", "javascript", "html", "prisma", "terraform", "markdown", "markdown_inline", "vim", "vimdoc", "java", "rust"},
 			highlight = { enable = true },
 			indent = { enable = true },
